d58e4d940e3da6222e37fdd559d38bbb765bfea0
Victor <josvictoronofre@gmail.com>
2024-06-12 12:02:12 -0300

    problema com o vs code

---
+++ src/br.com.dio.desafio/dominio/Main.java
@@ -0,0 +1,10 @@
+package br.com.dio.desafio.dominio;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        System.out.println(curso1);
+    }
+
+}
